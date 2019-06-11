1c200b99c04d4d410262c67f67ba0775b19e3afa
ahamedsajeer.15 <ahamedsajeer.15@cse.mrt.ac.lk>
2019-06-11 18:22:42 +0530

    Implement Kalman filter library

--- app/src/main/java/com/example/getkalmanfilteredloc/MainActivity.java
+++ app/src/main/java/com/example/getkalmanfilteredloc/MainActivity.java
@@ -1,36 +1,40 @@
 package com.example.getkalmanfilteredloc;
 
 import android.location.Location;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Toast;
 
-import mad.location.manager.lib.Commons.Utils;
 import mad.location.manager.lib.Interfaces.LocationServiceInterface;
 import mad.location.manager.lib.Services.KalmanLocationService;
 import mad.location.manager.lib.Services.ServicesHelper;
 
 public class MainActivity extends AppCompatActivity implements LocationServiceInterface {
 
+    private static final String TAG = "MAIN";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+        init();
         ServicesHelper.addLocationServiceInterface(this);
+    }
 
+    private void init() {
         ServicesHelper.getLocationService(this, value -> {
             if (value.IsRunning()) {
                 return;
             }
             value.stop();
-            KalmanLocationService.Settings settings = KalmanLocationService.defaultSettings;
-            value.reset(settings); //TODO!! here you can adjust your filter behavior by changing settings object
+            KalmanLocationService.Settings settings =KalmanLocationService.defaultSettings;
+            value.reset(settings);
             value.start();
         });
     }
@@ -38,5 +42,6 @@ public class MainActivity extends AppCompatActivity implements LocationServiceIn
     @Override
     public void locationChanged(Location location) {
         Toast.makeText(this, "Lat: "+location.getLatitude()+" Long: "+location.getLongitude(), Toast.LENGTH_SHORT).show();
+        Log.e(TAG, "locationChanged" );
     }
 }
